aeeee710780a83adbda619dddc06537a90731850
stahhl <arvostahl@gmail.com>
2019-06-24 15:08:13 +0200

    Add project files.

---
+++ ExcercisesMethods/Creating_Methods_1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ExcercisesMethods
+{
+    class Creating_Methods_1
+    {
+        static public void Start(bool reset)
+        {
+            if(reset == true)
+            {
+                Console.Clear();
+                Console.WriteLine("Module 1 loaded: Creating Methods: ");
+            }
+
+            Console.WriteLine("Enter a string of names seperated by comma: ");
+            List<string> separatedNames = GetNames(Console.ReadLine());
+            foreach (string s in separatedNames)
+            {
+                Program.PrintColoredMessage($"***{s.ToUpper()}***", ConsoleColor.Green);
+            }
+            Program.Restart();
+        }
+        static void ValidateInputString(string input)
+        {
+            if(input == string.Empty)
+            {
+                Program.PrintColoredMessage($"Empty input! ", ConsoleColor.Red);
+                Start(false);
+            }
+        }
+        static bool ValidateName(string name)
+        {
+            bool value = true;
+
+            if(name.Length < 2 || name.Length > 9)
+            {
+                Program.PrintColoredMessage($"A person can only have 2 to 9 letters! ", ConsoleColor.Red);
+                value = false;
+            }
+            foreach (char c in name)
+            {
+                if(char.IsLetter(c) == false)
+                {
+                    Program.PrintColoredMessage($"Couldn't convert {c} to a letter! ", ConsoleColor.Red);
+                    value = false;
+                }
+            }
+            return value;
+        }
+        static List<string> GetNames(string input)
+        {
+            ValidateInputString(input);
+            string[] stringArray = input.Split(",");
+            List<string> stringList = new List<string>();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                string stringFormatted = FormatString(stringArray[i]);
+
+                if(ValidateName(stringFormatted) == true)
+                {
+                    stringList.Add(stringArray[i]);
+                }
+                else
+                {
+                    Program.PrintColoredMessage($"The format on {stringArray[i]} is wrong wont be adding it! ", ConsoleColor.Red);
+                }
+            }
+            return stringList;
+        }
+        static string FormatString(string input)
+        {
+            return input.Trim().ToUpper();
+        }
+    }
+}
---
+++ ExcercisesMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExcercisesMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creating_Methods_1.Start(true);
+        }
+        static public void PrintColoredMessage(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        static public void Restart()
+        {
+            Console.WriteLine("Press any key to restart...");
+            Console.ReadLine();
+            Console.Clear();
+            Main(null);
+        }
+    }
+}
